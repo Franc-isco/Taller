fc83ee9184ea22e23a45c268d9c386ecf537266a
Francisco <francisco.tabilo05@inacapmail.cl>
2024-09-23 11:32:34 -0300

    usuario

--- Biblioteca/clases/usuario.py
+++ Biblioteca/clases/usuario.py
@@ -1,25 +1,12 @@
 class usuario:
-    def __init__(self, numero_identificacion, nombre, correo, telefono, habilitado):
-        self.__numero_identificacion = numero_identificacion
+    def __init__(self, n_identificacion, nombre, correo, telefono, habilitado):
+        self.__n_identificacion = n_identificacion
         self.__nombre = nombre
         self.__correo = correo
         self.__telefono = telefono
         self.__habilitado = habilitado
 
-    def obtener_numero_identificacion(self):
-        return self.__numero_identificacion
-    
-    def obtener_nombre(self):
-        return self.__nombre
-    
-    def obtener_correo(self):
-        return self.__correo
-    
-    def obtener_telefono(self):
-        return self.__telefono
-    
-    def obtener_habilitado(self):
-        return self.__habilitado
+
 
 
     
